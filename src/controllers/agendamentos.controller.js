b50f0d1ffeaa8de26b89d683da23b0dec2411136
Kamilla <kamilla.rodrigues@auvo.com.br>
2021-05-17 21:05:29 -0300

    sem condicional

--- src/controllers/agendamentos.controller.js
+++ src/controllers/agendamentos.controller.js
@@ -9,14 +9,13 @@ module.exports = {
   async create(req, res) {
     const { dia, hora, usuarioId } = req.body;
     let data = {};
-    let agendamento = await Agendamento.findOne({ dia, hora, usuarioId });
-
-    if (!agendamento) {
+    
+    try {
       data = { dia, hora, usuarioId };
       agendamento = await Agendamento.create(data);
       return res.status(200).json(agendamento);
-    } else {
-      return res.status(500).json(agendamento);
+    } catch (error) {
+      return res.status(500).json(error);
     }
   },
 }
